b887309017cc0b16466dfe66ca3ee7dd49170ab3
Erik <erik.langenborg@gmail.com>
2015-05-29 18:22:04 -0400

    timeline range selector graph

--- web/ts/graph/startend.ts
+++ web/ts/graph/startend.ts
@@ -5,25 +5,42 @@ module Graphs {
   export class StartEnd {
     protected d3GraphElement: D3._Selection<any>;
 
-    protected width: number = 0;
-    protected height: number = 0;
-    protected marginPx: number = 8;
+    private width: number = 0;
+    private height: number = 0;
+    private marginPx: number = 8;
+
+    private rangeStart: number = 0;
+    private rangeEnd: number = 0;
+
 
     private graphSvg: D3._Selection<any>;
     private xScale: D3.Scale.TimeScale;
+    private yScale: D3.Scale.LinearScale;
+    private areaRange: D3.Svg.Area;
 
     private static parseYear: (src: string) => Date = d3.time.format("%Y").parse;
 
     constructor(private id: string,
       private yearStart: number = 1960,
       private yearEnd: number = 2013) {
+
+      this.rangeStart = Math.floor((this.yearEnd - this.yearStart) / 2 + this.yearStart);
+      this.rangeEnd = Math.floor((this.yearEnd - this.yearStart) / 2 + this.yearStart) + 1;
+
       this.d3GraphElement = d3.select("#" + this.id);
       this.collectHeightWidth();
 
       this.xScale = d3.time.scale()
         .range([0, this.width])
         .nice();
-
+      this.yScale = d3.scale.linear()
+        .range([this.height, 0])
+        .domain([0, 1])
+        .nice();
+      this.areaRange = d3.svg.area()
+        .x((d) => { return this.xScale(StartEnd.parseYear(d + '')); })
+        .y0(this.height)
+        .y1((d) => { return 0.75; });
 
       this.graphSvg = this.d3GraphElement
         .attr("width", this.width + this.marginPx * 2)
@@ -31,13 +48,36 @@ module Graphs {
         .append("g")
         .attr("transform", "translate(" + this.marginPx + "," + this.marginPx + ")");
 
-
+      this.graphSvg.append("path")
+        .attr("class", "area alive");
 
       //debounce this
       d3.select(window).on('resize', this.resize);
       this.resize();
+
+      var clickHandler = this.handleClick;
+      this.d3GraphElement.on("click", function() {
+        var p1 = d3.mouse(this);
+        console.log(p1);
+        var x = p1[0];
+        clickHandler(x);
+      });
     }
 
+    private handleClick = (x: number): void => {
+      var clickedDate = this.xScale.invert(x);
+      var fullYear = clickedDate.getFullYear();
+      var distToStart = Math.abs(this.rangeStart - fullYear);
+      var distToEnd = Math.abs(this.rangeEnd - fullYear);
+      if (distToStart < distToEnd) {
+        //closer to start
+        this.rangeStart = fullYear;
+      } else {
+        //closer to end
+        this.rangeEnd = fullYear;
+      }
+      this.handleNewYears(null, 0.2);
+    }
 
     protected resize = (): void => {
       this.collectHeightWidth();
@@ -67,9 +107,11 @@ module Graphs {
         StartEnd.parseYear(this.yearStart + ''),
         StartEnd.parseYear(this.yearEnd + '')
       ]);
+      var tickCount = Math.max(Math.floor((this.width - 20) / 150),1);
       var xAxis = d3.svg.axis()
         .scale(this.xScale).orient("top")
-        .ticks(3);
+        .tickPadding(4)
+        .ticks(tickCount);
       // if no axis exists, create one, otherwise update it
       if (this.graphSvg.selectAll(".x.axis")[0].length < 1) {
         this.graphSvg.append("g")
@@ -81,8 +123,15 @@ module Graphs {
       } else {
         this.graphSvg.selectAll(".x.axis").transition().duration(durationMs)
           .attr("transform", "translate(0," + this.height + ")")
-          .call(xAxis);
+          .call(xAxis)
+          .selectAll("text")
+          .style("text-anchor", "start");
       }
+
+
+      this.areaRange.y0(this.height); //adjust the fill region of the area
+      var t0 = this.graphSvg.transition().duration(durationMs);
+      t0.select(".area.alive").attr("d", this.areaRange([this.rangeStart, this.rangeEnd]));
     }
   }
 }
