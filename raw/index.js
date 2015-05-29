5bb07274514d5b40f47936453ed59380cf259c05
Erik <erik.langenborg@gmail.com>
2015-05-28 22:57:13 -0400

    Proper handling of data into flat csv file

--- raw/index.js
+++ raw/index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var csvParse = require("csv-parse");
+var csvStringify = require("csv-stringify");
 var R = require('ramda');
 
 var upperSet = fs.readFileSync('./outlays.function.fix.csv').toString();
@@ -28,10 +29,16 @@ var validDataEntry = R.and(validYearEntry, validValueEntry);
 var filterBadValues = R.filter(validDataEntry);
 var dataSetLengths = R.map(R.pipe(R.prop('data'), arrayLength));
 
+var cullDashesRegExp = /-*/g;
+var cullDashes = function (str) { return str.replace(cullDashesRegExp, ''); };
+var toCsvName = function (dataElm) {
+  return dataElm.superFunction + '/' + dataElm.normalFunction + '/' + dataElm.subFunction;
+}
+
 var createNodeEntryFromRowBase = R.curry(function (forgeDataSet,row) {
-  var superFunction = row[0];
-  var normalFunction = row[1];
-  var subFunction = row[2];
+  var superFunction = cullDashes(row[0]);
+  var normalFunction = cullDashes(row[1]);
+  var subFunction = cullDashes(row[2]);
   var dataSet = forgeDataSet(row);
   if (dataSet.length < 2) return false;
   return {
@@ -43,23 +50,59 @@ var createNodeEntryFromRowBase = R.curry(function (forgeDataSet,row) {
 });
 
 
-var processData = function (json) {
+var createSets = function (json) {
   var years = fixYears(json[0]);
-  var forgeDataSet = R.pipe(R.zipWith(forgeDataEntry, years),filterBadValues);
-  
+  var forgeDataSet = R.pipe(R.zipWith(forgeDataEntry, years), filterBadValues);
+
   var createNodes = R.map(createNodeEntryFromRowBase(forgeDataSet));
   var dat = nonEmptyFirstElm(json);
   var nodeSets = filterFalsy(createNodes(dat));
   
   // check for bad sets
-  var lengths = dataSetLengths(nodeSets); 
+  var lengths = dataSetLengths(nodeSets);
   var badSets = R.any(R.pipe(R.eq(lengths[0]), R.not), lengths);
   if (badSets) { console.warn('Dataset length mismatch'); }
-  
+
   return nodeSets;
 };
 
+var createElmInYearLine = function (yearNumber, setter) {
+  var extract = R.pipe(
+    R.prop('data'),
+    R.find(R.propEq('year', yearNumber)),
+    R.prop('value')
+  );
+  return extract(setter);
+//  return R.prepend(yearNumber, mainValues);
+};
+
+var processData = function (json) {
+  var sets = createSets(json);
+  
+  //create header column
+  var headers = R.prepend('date',R.map(toCsvName, sets));
+  console.log(headers);
+  
+  var years = R.map(R.prop('year'), sets[0].data);
+  console.log(years);
+  
+  var dataLines = R.map(function (thisYear) {
+    var createYearLineThis = R.curry(createElmInYearLine)(thisYear);
+    var rawLine = R.map(createYearLineThis, sets);
+    return R.prepend(thisYear, rawLine); 
+  },years);
+  console.log(dataLines);
+  
+  var fullCsv = R.prepend(headers, dataLines);
+  
+  return fullCsv;
+};
+
 csvParse(upperSet, function (err, data) {
-  var dat = JSON.stringify(processData(data));
+  var fixedData = processData(data);
+  var dat = JSON.stringify(fixedData);
+  var csv = csvStringify(fixedData, function (err, output) {
+    fs.writeFileSync('./budget.out.csv', output);
+  });
   fs.writeFileSync('./budget.json', dat);
 });
