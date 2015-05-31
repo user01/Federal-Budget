033a0b27d27576d311c59ec89e6e8d887c5c526d
Erik <erik.langenborg@gmail.com>
2015-05-31 16:45:45 -0400

    Loading all the data

--- web/ts/utility/data.all.ts
+++ web/ts/utility/data.all.ts
@@ -6,10 +6,32 @@
 declare var R: any;
 
 module Utility {
-  interface DataSets { gdp: DataSet, cpi:DataSet, population:DataSet, budget:DataSet };
+  interface DataSets { gdp: DataSet, cpi: DataSet, population: DataSet, budget: DataSet };
   export class DataAll extends CbBase {
-    constructor() {
+    public sets: DataSets;
+    
+    private static allReadySets = R.all((ds: DataSet): boolean=> { return ds.Ready; });
+    
+    constructor(private rootPath:string='') {
       super();
+    }
+
+    public Initialize = (): void => {
+      this.sets = {
+        gdp: new DataSet(this.rootPath + '/gdp.json'),
+        cpi: new DataSet(this.rootPath + '/cpi.json'),
+        population: new DataSet(this.rootPath + '/population.json'),
+        budget: new DataRich(this.rootPath + '/budget.json'),
+      };
+      R.values(this.sets).forEach((ds: DataSet) => {
+        ds.on('data', this.dataLoaded);
+        ds.Initialize();
+      });
+    }
+    
+    private dataLoaded = (): void => {
+      if (!DataAll.allReadySets(R.values(this.sets))) return;
+      console.log('All ready!', this.sets);
     }
   }
 }
