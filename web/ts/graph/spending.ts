a26050f3d8c3a53406caf0b282d65b5c0605f5b7
Erik <erik.langenborg@gmail.com>
2015-05-30 22:59:10 -0400

    Demo nodes working

--- web/ts/graph/spending.ts
+++ web/ts/graph/spending.ts
@@ -12,6 +12,8 @@ module Graph {
     private height: number = 0;
     private marginPx: number = 8;
     private backdrop: D3._Selection<any>;
+    private nodes: D3.Layout.GraphNodeForce[];
+    private force: D3.Layout.ForceLayout;
 
     constructor(private id: string) {
       super();
@@ -26,82 +28,84 @@ module Graph {
         .range(["hsl(180,100%,10%)", "hsl(210,100%,90%)"])
         .interpolate(d3.interpolateHsl);
 
-      var force = d3.layout.force()
+      this.force = d3.layout.force()
         .gravity(0)
         .charge(0)
         .size([this.width, this.height]);
 
-      var nodes = force.nodes();
+      this.nodes = this.force.nodes();
 
 
       this.backdrop = this.d3GraphElement.append("svg:rect")
-        .attr('class','backdrop')
+        .attr('class', 'backdrop')
         .attr("width", this.width)
         .attr("height", this.height);
 
-      force.on("tick", () => {
-        //  var k = e.alpha * .1;
-        //  nodes.forEach(function(node) {
-        //    node.y += (node.cy - node.y) * k;
-        //  });
+      this.force.on("tick", (e:any) => {
+          var k = e.alpha * .1;
+          this.nodes.forEach((node:any) => {
+            node.y += (node.cy - node.y) * k;
+          });
 
         this.d3GraphElement.selectAll("circle")
           .attr("cx", function(d) { return d.x; })
           .attr("cy", function(d) { return d.y; });
       });
 
-      var p0,height = this.height;
+      var p0, height = this.height;
+      var evtFn = this.mouseEvent;
 
       this.d3GraphElement.on("mousemove", function() {
         var p1 = d3.mouse(this);
-        //console.log(a,p1);
-
-//        var node = {
-//          x: p1[0],
-//          y: p1[1],
-//          px: (p0 || (p0 = p1))[0],
-//          py: p0[1],
-//          cy: Math.random() * (height - 200) + 100
-//        };
-//
-//        p0 = p1;
-//
-//                this.d3GraphElement.append("svg:circle")
-//                  .data([node])
-//                  .attr("cx", function(d) { return d.x; })
-//                  .attr("cy", function(d) { return d.y; })
-//                  .attr("r", 15)
-//                  .style("fill", function(d) { return color(d.cy); })
-//                  .transition()
-//                  .delay(3000)
-//                  .attr("r", 1e-6)
-//                  .each("end", function() { nodes.shift(); })
-//                  .remove();
-//
-//        nodes.push(node);
-//        force.start();
+        evtFn(p1);
       });
 
 
-
-      d3.select(window).on('resize.'+this.id, this.resize);
+      d3.select(window).on('resize.' + this.id, this.resize);
       this.resize();
 
     }
-    
+
+    private mouseEvent = (pt: number[]) => {
+
+      var node = {
+        x: pt[0],
+        y: pt[1],
+        px: pt[0],
+        py: pt[1],
+        cy: Math.random() * (this.height - 200) + 100
+      };
+
+
+      this.d3GraphElement.append("svg:circle")
+        .data([node])
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .attr("r", 15)
+//        .style("fill", function(d) { return color(d.cy); })
+        .transition()
+        .delay(3000)
+        .attr("r", 1e-6)
+        .each("end", () => { this.nodes.shift(); })
+        .remove();
+
+      this.nodes.push(node);
+      this.force.start();
+    }
+
     protected resize = (): void => {
       this.collectHeightWidth();
 
-//      this.graphSvg
-//        .attr("width", this.width + this.marginPx * 2)
-//        .attr("height", this.height + this.marginPx * 2)
+      //      this.graphSvg
+      //        .attr("width", this.width + this.marginPx * 2)
+      //        .attr("height", this.height + this.marginPx * 2)
       //this.handleNewYears(null, 0);
-//      console.log(this.width, this.height);
+      //      console.log(this.width, this.height);
       this.backdrop.attr("width", this.width)
         .attr("height", this.height);
     }
 
-    
+
     protected collectHeightWidth = (): void => {
       this.width = parseInt(this.d3GraphElement.style("width")) - this.marginPx * 2;
       this.height = parseInt(this.d3GraphElement.style("height")) - this.marginPx * 2;
