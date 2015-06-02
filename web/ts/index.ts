765aaf696aa7d193115618d51f2795af4a70ec2a
Erik <erik.langenborg@gmail.com>
2015-06-02 16:30:08 -0400

    Add basic year range feedback

--- web/ts/index.ts
+++ web/ts/index.ts
@@ -7,10 +7,12 @@ declare var R:any;
 
 console.log('System online');
 
+var yearFrom = document.getElementById('year-from');
+var yearTo = document.getElementById('year-to');
 
 //start/end tooly graph
 var startend = new Graph.StartEnd('graph-timeline');
-startend.on('range', (newRange): void=> { console.log(newRange); });
+//startend.on('range', (newRange): void=> { console.log(newRange); });
 
 var spending:Graph.Spending;
 var dataAll = new Utility.DataAll('data');
@@ -20,6 +22,8 @@ dataAll.on('data', (data: Utility.DataSets): void=> {
   startend.on('range', (newRange): void=> {
     spending.YearFrom = newRange.start;
     spending.YearTo = newRange.end;
+    yearFrom.textContent = newRange.start;
+    yearTo.textContent = newRange.end;
     spending.RenderNewState();
   });
   
