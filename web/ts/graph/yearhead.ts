b98c11f6ba0dd369a2ef991a0a7bf8f71213c0c7
Erik <erik.langenborg@gmail.com>
2015-07-03 01:21:18 -0400

    Properly set current year on start

--- web/ts/graph/yearhead.ts
+++ web/ts/graph/yearhead.ts
@@ -30,7 +30,7 @@ module Graph {
 
       this.d3GraphElement = d3.select("#" + this.id);
       this.collectHeightWidth();
-      this.targetYear = Math.floor((this.yearEnd - this.yearStart) / 2 + this.yearStart);
+      this.targetYear = this.yearEnd;
       this.currentYear = this.targetYear;
 
       this.xScale = d3.time.scale()
